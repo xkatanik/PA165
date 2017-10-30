9adb1c1d5160b84cea71b190fc5abee8f2728f48
xkatanik <k.katanik@gmail.com>
2017-10-30 16:48:29 +0100

    Fixed test and implemented sellCar

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -29,8 +30,22 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 	}
 
 	@Override
-	public void addCarToStorage(Car car) {
-		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		try {
+			CarShopStorage.getInstancce().getCars().remove(car);
+		} catch (Exception e){
+			throw new RequestedCarNotFoundException("Car is not in the storage");
+		}
+
+	}
+
+	@Override
+	public void addCarToStorage(Car car) throws IllegalArgumentException{
+		if(car.getPrice() < 0){
+			throw new IllegalArgumentException("Price cannot be negative");
+		} else {
+			CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
+		}
 	}
 
 }
